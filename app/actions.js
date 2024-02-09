c2b2136ee588c7b19f1dd3965172fed8491a8ce0
Zoe <19900057+vpsx@users.noreply.github.com>
2024-02-09 11:16:28 -0500

    Refactor validate token function

--- app/actions.js
+++ app/actions.js
@@ -1,6 +1,30 @@
+var jwt = require("jsonwebtoken")
+
 export async function getPublicKey() {
   // Response is cached by default.
   const response = await fetch(process.env.AUTH_PUBLICKEYS_URL);
   const result = await response.json();
   return result[0]
+}
+
+export async function validateAuthToken(token) {
+	// Get auth provider's public key for validation
+	try {
+		var publickey = await getPublicKey()
+	} catch (err) {
+		const e = new Error("Could not fetch auth provider public keys", { cause: err });
+		e.httpcode = 500;
+		throw e;
+	}
+
+	// Validate token
+	try {
+	    var data = jwt.verify(token,publickey);
+	} catch(err) {
+	    const e = new Error("Token is missing or is not valid", { cause: err });
+	    e.httpcode = 401;
+	    throw e;
+	}
+
+	return data
 }
--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,9 +1,8 @@
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-import { getPublicKey } from '../../actions.js'
+import { validateAuthToken } from '../../actions.js'
 
-var jwt = require("jsonwebtoken")
 
 
 // Get token from query parameters, validate token, set cookie, redirect.
@@ -12,23 +11,11 @@ export async function GET(request) {
 	// Get token from query parameters
 	const token = request.nextUrl.searchParams.get('token')
 
-	// Get auth provider's public key for validation
+	// Validate token
 	try {
-	  const publickey = await getPublicKey()
-
-	  // Validate token
-      try {
-        var _ = jwt.verify(token,publickey);
-      } catch(err) {
-	    return new Response("Token is missing or is not valid: " + err, {
-		  status: 401,
-	    })
-      }
-
-	} catch (err) {
-	  return new Response("Could not fetch auth provider public keys: " + err, {
-		  status: 500,
-	  })
+		await validateAuthToken(token)
+	} catch(err) {
+	    return new Response(err, { status: err.httpcode })
 	}
 
     // JWT is valid; set cookie
