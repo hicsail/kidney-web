79a73dd0d596a4103ce31e9576161dbe2e448fd0
Zoe <19900057+vpsx@users.noreply.github.com>
2024-02-09 14:57:05 -0500

    Add logout functionality

--- app/page.js
+++ app/page.js
@@ -13,6 +13,23 @@ export default async function Home() {
 			return `Hello, ${user.email}`;
 		}
 	}
+	function LoginLogout() {
+		if (!user) {
+			return (
+				<a
+					href={
+						process.env.AUTH_LOGIN_URL +
+						"/?projectId=" +
+						process.env.AUTH_PROJECTID
+					}
+				>
+					Log in with SAIL test auth
+				</a>
+			);
+		} else {
+			return <a href="/auth/logout">Log out</a>;
+		}
+	}
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -41,17 +58,7 @@ export default async function Home() {
 			</div>
 
 			<h1>{GreetingText()}</h1>
-			<h1>
-				<a
-					href={
-						process.env.AUTH_LOGIN_URL +
-						"/?projectId=" +
-						process.env.AUTH_PROJECTID
-					}
-				>
-					Log in with SAIL test auth
-				</a>
-			</h1>
+			<h1>{LoginLogout()}</h1>
 			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
 
 			<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
